f9a891bbbfee6c232a41b47d5ad23cb4e885c1fc
Zhongmiao Li <mars.leezm@gmail.com>
2015-10-11 12:21:04 +0100

    Add more output

--- plugins/antidote/src/main/java/org/radargun/cachewrappers/TransactionManager.java
+++ plugins/antidote/src/main/java/org/radargun/cachewrappers/TransactionManager.java
@@ -77,6 +77,7 @@ public class TransactionManager {
 				connection.send(MSG_SingleUpReq, singleUpReq);
 				log.info("Sent message");
 				FpbPrepTxnResp resp = FpbPrepTxnResp.parseFrom(connection.receive(MSG_PrepTxnResp));
+				log.info("Got resp reply!");
 				if (!resp.getSuccess())
 					throw new Exception();
 			}
